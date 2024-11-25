5a61d6b622636d92527834f60b3c6900316167b6
Taegyun Kim <taegyun.kim@datadoghq.com>
2024-11-25 16:08:30 -0500

    remove prints

--- echion/tasks.h
+++ echion/tasks.h
@@ -19,7 +19,6 @@
 #endif
 
 #include <exception>
-#include <iostream>
 #include <mutex>
 #include <stack>
 #include <unordered_map>
@@ -169,8 +168,6 @@ TaskInfo::TaskInfo(TaskObj *task_addr)
     try
     {
         name = string_table.key(task.task_name);
-        std::cout << "Task name: " << string_table.lookup(name) << std::endl;
-
     }
     catch (StringTable::Error &)
     {
